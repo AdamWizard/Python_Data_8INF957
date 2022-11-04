e5a5b477b7589fa271bc2ae7c0bc839f3bf5031f
Banalian <64379456+Banalian@users.noreply.github.com>
2022-11-04 15:16:04 -0400

    feat(extract): extract line by label

--- data_analysis.py
+++ data_analysis.py
@@ -10,9 +10,18 @@ def import_data(file_path: str) -> pd.DataFrame:
     return data
 
 
+def extract_lines_by_label(data: pd.DataFrame, label: int) -> pd.DataFrame:
+    # Extract the lines with the column "label" equal to the label parameter
+    return data[data['Label'] == label]
+
+
 def main():
     # Import the data
     data = import_data("data/copd_1100_train.csv")
+    
+    # Extract the lines with the label 4
+    data = extract_lines_by_label(data, 4)
+
     # Print the data
     print(data)
 
