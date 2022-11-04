779b3cc61ba6d90913bd7b963b1a02d8a992af4c
Lilian Pouvreau <64379456+Banalian@users.noreply.github.com>
2022-11-03 20:07:45 -0400

    feat(gdata analysis): add file and import function

---
+++ data_analysis.py
@@ -0,0 +1,17 @@
+# Data analysis file. Contains code to show an example of how you can make data analysis (using pandas for example)
+
+# Import the pandas library
+import pandas as pd
+
+
+def import_data(file_path: str) -> pd.DataFrame:
+    # Import the data from the csv file
+    data = pd.read_csv(file_path)
+    return data
+
+
+def main():
+    # Import the data
+    data = import_data("data/copd_1100_train.csv")
+    # Print the data
+    print(data)
